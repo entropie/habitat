380a599b04748d1e73db6af101e9c852f05aedeb
Michael Trommer <mictro@gmail.com>
2020-07-23 15:28:31 +0200

    redirect to snippets because no dashboard

--- plugins/backend/controllers/dashboard/index.rb
+++ plugins/backend/controllers/dashboard/index.rb
@@ -3,6 +3,9 @@ module Backend::Controllers::Dashboard
     include Api::Action
 
     def call(params)
+      Habitat.plugin_enabled?(:snippets) do
+        redirect_to routes.snippets_path
+      end
     end
   end
 end
