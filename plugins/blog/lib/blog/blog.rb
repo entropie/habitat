9671e634a5fa4e3130ef3dde90d1e1c2f692acff
Michael Trommer <mictro@gmail.com>
2020-08-10 01:21:26 +0200

    module info module in own file

--- plugins/blog/lib/blog/blog.rb
+++ plugins/blog/lib/blog/blog.rb
@@ -7,6 +7,7 @@
 require_relative "database"
 require_relative "images"
 require_relative "post"
+require_relative "module_info"
 require_relative "filter"
 require_relative "templates"
 
@@ -34,93 +35,6 @@ module Blog
 
     def posts_sorted(*args, sort_by: -> (post) { post.created_at }, &blk)
       blog.posts(*args, blk).sort_by(&sort_by).reverse
-    end
-
-  end
-
-
-
-
-  module BackendModuleInfo
-
-    class Info
-      def self.modules
-        @modules ||= []
-      end
-      
-      def self.inherited(o)
-        modules << o
-      end
-
-      def initialize(post)
-        @post = post
-      end
-
-      def ident
-        self.class.to_s.split("::").last.downcase.gsub(/info$/, "")
-      end
-
-      def to_html
-        "<div class='post-info mod#{ident} text-ellipsis'><strong>#{ident}</strong> <span>%s</span></div>"
-      end
-
-      def plugin_activated?(plug)
-        Habitat.quart.plugins.activated?(plug)
-      end
-    end
-
-    class UserInfo < Info
-      def user
-        @user ||= Habitat.adapter(:user).by_id(@post.user_id)
-      end
-      
-      def to_html
-        if Habitat.quart.plugins.enabled?(:user)
-          super % user.name
-        else
-          ""
-        end
-      end
-    end
-
-
-    class VGWortInfo < Info
-
-      def post
-        @post.with_plugin(VGWort)
-      end
-      
-      def to_html
-        ret = ""
-        return "" unless Habitat.quart.plugins.enabled?(:vgwort)
-        
-        if post.vgwort.id_attached?
-          ret = "#{post.vgwort.code}"
-        else
-          ret = "<span class='text-warning'>unset</span>"
-        end
-        super % ret
-      end
-    end
-
-
-    class LanguagesInfo < Info
-
-      def to_html
-        langs = @post.languages
-        return "" if langs.empty?
-        super % langs.map {|l| "<a href='#{Blog.routes.post_path(@post.slug, l)}'>#{l}</a>"}
-      end
-
-    end
-    
-    class TemplateInfo < Info
-      def to_html
-        template = @post.template
-        if template
-          super % @post.template
-        end
-      end
     end
 
   end
