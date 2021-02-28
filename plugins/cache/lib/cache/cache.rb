7357c50c4ab1293872244eb20c8bf40ae8b3ef0a
Michael Trommer <mictro@gmail.com>
2021-02-28 21:54:28 +0100

    no $anymore

--- plugins/cache/lib/cache/cache.rb
+++ plugins/cache/lib/cache/cache.rb
@@ -1,20 +1,25 @@
-$cache = Memcached.new("localhost:11211")
+#$cache = Memcached.new("localhost:11211")
 
 module Cache
 
+  def self.cache
+    @cache ||= Memcached.new("localhost:11211")
+  end
+  
   def self.cache_key(k)
     "%s-%s" % [Habitat.quart.identifier, k]
   end
 
   def self.[](obj)
-    ret = $cache.get(cache_key(obj)) rescue nil
+    ret = cache.get(cache_key(obj)) rescue nil
     Habitat.log :debug, "get Cache(%s, '%s')" % [ cache_key(obj), ret]
     ret
   end
 
   def self.[]=(obj, value)
     Habitat.log :debug, "set Cache(%s, '%s')" % [ cache_key(obj), value]
-    $cache.set(cache_key(obj), value)
+    cache.set(cache_key(obj), value)
+    value
   end
 
 end
