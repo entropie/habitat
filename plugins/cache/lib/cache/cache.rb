9b0debee063e77b1cc338f3b16941980ce7d8e87
Michael Trommer <mictro@gmail.com>
2021-03-10 01:15:03 +0100

    reread cache when no result (may be gone)

--- plugins/cache/lib/cache/cache.rb
+++ plugins/cache/lib/cache/cache.rb
@@ -13,6 +13,12 @@ module Cache
   def self.[](obj)
     ret = cache.get(cache_key(obj)) rescue nil
     Habitat.log :debug, "get Cache(%s, '%s')" % [ cache_key(obj), ret]
+
+    unless ret
+      Habitat.log :debug, "cached key not found; #{cache_key(obj)} reading again"
+      T.read
+      ret = cache.get(cache_key(obj))
+    end
     ret
   end
 
