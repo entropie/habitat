6135c80dad7a08a5b42e28bd7a1dd70ac50b9db1
Michael Trommer <mictro@gmail.com>
2022-08-19 03:46:18 +0200

    no debug message

--- plugins/snippets/lib/snippets/snippets.rb
+++ plugins/snippets/lib/snippets/snippets.rb
@@ -120,7 +120,7 @@ module Snippets
 
     def active_path(path)
       rp = locals[:request_path]
-      p "%s - %s" % [rp, path]
+      #p "%s - %s" % [rp, path]
       if rp.include?("/s/") and path.include?("/s/") and rp.include?(path)
         true
       elsif rp =~ /^#{path}\//
