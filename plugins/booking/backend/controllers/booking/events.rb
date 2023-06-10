a8645474ceec0168ee1fcdbcb067cda362ba674e
Michael Trommer <mictro@gmail.com>
2023-06-10 21:31:38 +0200

    show upcoming events first

--- plugins/booking/backend/controllers/booking/events.rb
+++ plugins/booking/backend/controllers/booking/events.rb
@@ -6,6 +6,8 @@ module Backend::Controllers::Booking
 
     def call(params)
       @events = booking.events_all
+                  .sort_by{ |ev| ev.start_date }.reverse
+
       @pager = Pager::BackendPager.new(params, @events, 10)
       @pager.link_proc = -> (n) { routes.eventsPager_path(n) } 
     end
